9ed9395086034487d2c9673f3778fd2a0a27bcc3
adam <financeCoding@gmail.com>
2013-04-14 10:44:16 -0700

    Testing new hop tasks

--- tool/build_deploy.sh
+++ tool/build_deploy.sh
@@ -2,6 +2,9 @@ pub install &&
 dart tool/hop_runner.dart --log-level all clean &&
 dart tool/hop_runner.dart --log-level all dart2js && 
 dart tool/hop_runner.dart --log-level all dart2js_dart &&
-dart tool/hop_runner.dart --log-level all deploy &&
-dart tool/hop_runner.dart --log-level all compress &&
-dart tool/hop_runner.dart --log-level all clean
+dart tool/hop_runner.dart --log-level all deploy_heroku &&
+dart tool/hop_runner.dart --log-level all create_webapp &&
+dart tool/hop_runner.dart --log-level all deploy_gae &&
+dart tool/hop_runner.dart --log-level all create_gae
+# && dart tool/hop_runner.dart --log-level all compress 
+# && dart tool/hop_runner.dart --log-level all clean
--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -9,12 +9,79 @@ import 'package:hop/hop_tasks.dart';
  * https://gist.github.com/darktable/873098
  */
 
+/*
+mkdir stream_todomvc
+cd stream_todomvc
+heroku create stream-todomvc
+heroku config:add BUILDPACK_URL=https://github.com/igrigorik/heroku-buildpack-dart.git
+git init
+git remote add heroku git@heroku.com:stream-todomvc.git
+ */
+
+String server_dart = 
+"""
+// cat web/webapp/server.dart 
+library server;
+
+import 'dart:io';
+import "package:stream/stream.dart";
+
+void main() {
+  var port = Platform.environment.containsKey('PORT') ? int.parse(Platform.environment['PORT']) : 8080;
+  var host = '0.0.0.0';
+  var streamServer = new StreamServer();
+  streamServer
+  ..port = port
+  ..host = host
+  ..start();
+}
+""";
+
+String proc_file =
+"""
+web: ./dart-sdk/bin/dart --package-root=./packages/ web/webapp/server.dart
+""";
+
 void main() {
   _assertKnownPath();
   final paths = ['web/sunflower.dart'];
   addTask('dart2js', createDart2JsTask(paths, minify: true, liveTypeAnalysis: true, rejectDeprecatedFeatures: true));
   addAsyncTask('dart2js_dart', (ctx) => startProcess(ctx, 'dart2js', ['--output-type=dart', '--minify','-oweb/sunflower.dart','web/sunflower.dart']));
-  addAsyncTask('deploy', (ctx) => startProcess(ctx, 'rsync', ['-RLr', 'web/', 'deploy/']));
+  
+  addTask('deploy_heroku', copyDirectory("web", "deploy_heroku/web", followLinks: true));
+  addAsyncTask('create_webapp', (ctx) {
+    // dart --package-root=../packages/ web/webapp/server.dart
+    var completer = new Completer();
+    File outFile = new File("deploy_heroku/web/webapp/server.dart");
+    Directory outDir = new Directory("deploy_heroku/web/webapp/");
+    outDir.createSync(recursive: true);
+    outFile.writeAsStringSync(server_dart);
+    
+    outFile = new File("deploy_heroku/Procfile");
+    outFile.writeAsStringSync(proc_file);
+    
+    completer.complete(true);
+    return completer.future;
+  });
+  
+  addTask('deploy_gae', copyDirectory("web", "deploy_gae/static", followLinks: true));
+  addAsyncTask('create_gae', (ctx) { 
+    var completer = new Completer();
+    
+    File outFile = new File("deploy_gae/App.yaml");
+    outFile.writeAsStringSync(gae_app_yaml);
+    
+    completer.complete(true);
+    return completer.future;
+  });
+  
+  //
+  // gh_pages
+  //
+  addTask('deploy_github', copyDirectory("web", "deploy_github", followLinks: true));
+  addAsyncTask('deploy_gh_pages', (ctx) => branchForDir(ctx, 'master', 'deploy_github', 'gh-pages'));
+  
+  //addAsyncTask('deploy', (ctx) => startProcess(ctx, 'rsync', ['-RLr', 'web/', 'deploy/']));
   addAsyncTask('compress', (ctx) => startProcess(ctx, 'tar', ['-zcvf', 'deploy.tar.gz', '-C', 'deploy/web/', '.']));
   addAsyncTask('clean', (ctx) => startProcess(ctx, 'rm', ['-rf', 'deploy']));
   runHop();  
@@ -28,3 +95,16 @@ void _assertKnownPath() {
   final thisFile = new File('tool/hop_runner.dart');
   assert(thisFile.existsSync());
 }
+
+String gae_app_yaml = 
+"""
+application: you-app-name-here
+version: 1
+runtime: python
+api_version: 1
+
+handlers:
+- url: /(.*)
+  static_files: static/\\1
+  upload: static/(.*)
+""";
