f13bb38650954abaeeb1517dec2cab79f9852225
adam <financeCoding@gmail.com>
2013-04-14 11:06:38 -0700

    Update hop runner

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -18,7 +18,7 @@ git init
 git remote add heroku git@heroku.com:stream-todomvc.git
  */
 
-String server_dart = 
+String server_dart =
 """
 // cat web/webapp/server.dart 
 library server;
@@ -42,61 +42,8 @@ String proc_file =
 web: ./dart-sdk/bin/dart --package-root=./packages/ web/webapp/server.dart
 """;
 
-void main() {
-  _assertKnownPath();
-  final paths = ['web/sunflower.dart'];
-  addTask('dart2js', createDart2JsTask(paths, minify: true, liveTypeAnalysis: true, rejectDeprecatedFeatures: true));
-  addAsyncTask('dart2js_dart', (ctx) => startProcess(ctx, 'dart2js', ['--output-type=dart', '--minify','-oweb/sunflower.dart','web/sunflower.dart']));
-  
-  addTask('deploy_heroku', copyDirectory("web", "deploy_heroku/web", followLinks: true));
-  addAsyncTask('create_webapp', (ctx) {
-    // dart --package-root=../packages/ web/webapp/server.dart
-    var completer = new Completer();
-    File outFile = new File("deploy_heroku/web/webapp/server.dart");
-    Directory outDir = new Directory("deploy_heroku/web/webapp/");
-    outDir.createSync(recursive: true);
-    outFile.writeAsStringSync(server_dart);
-    
-    outFile = new File("deploy_heroku/Procfile");
-    outFile.writeAsStringSync(proc_file);
-    
-    completer.complete(true);
-    return completer.future;
-  });
-  
-  addTask('deploy_gae', copyDirectory("web", "deploy_gae/static", followLinks: true));
-  addAsyncTask('create_gae', (ctx) { 
-    var completer = new Completer();
-    
-    File outFile = new File("deploy_gae/App.yaml");
-    outFile.writeAsStringSync(gae_app_yaml);
-    
-    completer.complete(true);
-    return completer.future;
-  });
-  
-  //
-  // gh_pages
-  //
-  addTask('deploy_github', copyDirectory("web", "deploy_github", followLinks: true));
-  addAsyncTask('deploy_gh_pages', (ctx) => branchForDir(ctx, 'master', 'deploy_github', 'gh-pages'));
-  
-  //addAsyncTask('deploy', (ctx) => startProcess(ctx, 'rsync', ['-RLr', 'web/', 'deploy/']));
-  addAsyncTask('compress', (ctx) => startProcess(ctx, 'tar', ['-zcvf', 'deploy.tar.gz', '-C', 'deploy/web/', '.']));
-  addAsyncTask('clean', (ctx) => startProcess(ctx, 'rm', ['-rf', 'deploy']));
-  runHop();  
-}
-
-
-void _assertKnownPath() {
-  // since there is no way to determine the path of 'this' file
-  // assume that Directory.current() is the root of the project.
-  // So check for existance of /bin/hop_runner.dart
-  final thisFile = new File('tool/hop_runner.dart');
-  assert(thisFile.existsSync());
-}
-
-String gae_app_yaml = 
+// TODO: make function that inserts the application name.
+String gae_app_yaml =
 """
 application: you-app-name-here
 version: 1
@@ -108,3 +55,63 @@ handlers:
   static_files: static/\\1
   upload: static/(.*)
 """;
+
+void main() {
+  _assertKnownPath();
+  final paths = ['web/sunflower.dart'];
+  addTask('dart2js', createDart2JsTask(paths, minify: true, liveTypeAnalysis: true, rejectDeprecatedFeatures: true));
+  addAsyncTask('dart2js_dart', (ctx) => startProcess(ctx, 'dart2js', ['--output-type=dart', '--minify','-oweb/sunflower.dart','web/sunflower.dart']));
+
+  //
+  // heroku
+  //
+  addTask('deploy_heroku', copyDirectory("web", "deploy_heroku/web", followLinks: true));
+  addAsyncTask('create_webapp', (ctx) {
+    // dart --package-root=../packages/ web/webapp/server.dart
+    var completer = new Completer();
+    File outFile = new File("deploy_heroku/web/webapp/server.dart");
+    Directory outDir = new Directory("deploy_heroku/web/webapp/");
+    outDir.createSync(recursive: true);
+    outFile.writeAsStringSync(server_dart);
+
+    outFile = new File("deploy_heroku/Procfile");
+    outFile.writeAsStringSync(proc_file);
+
+    completer.complete(true);
+    return completer.future;
+  });
+
+  //
+  // google app engine
+  //
+  addTask('deploy_gae', copyDirectory("web", "deploy_gae/static", followLinks: true));
+  addAsyncTask('create_gae', (ctx) {
+    var completer = new Completer();
+
+    File outFile = new File("deploy_gae/App.yaml");
+    outFile.writeAsStringSync(gae_app_yaml);
+
+    completer.complete(true);
+    return completer.future;
+  });
+
+  //
+  // github gh_pages
+  //
+  addTask('deploy_github', copyDirectory("web", "deploy_github", followLinks: true));
+  addAsyncTask('deploy_gh_pages', (ctx) => branchForDir(ctx, 'master', 'deploy_github', 'gh-pages'));
+
+  //addAsyncTask('deploy', (ctx) => startProcess(ctx, 'rsync', ['-RLr', 'web/', 'deploy/']));
+  addAsyncTask('compress', (ctx) => startProcess(ctx, 'tar', ['-zcvf', 'deploy.tar.gz', '-C', 'deploy/web/', '.']));
+  addAsyncTask('clean', (ctx) => startProcess(ctx, 'rm', ['-rf', 'deploy']));
+  runHop();
+}
+
+
+void _assertKnownPath() {
+  // since there is no way to determine the path of 'this' file
+  // assume that Directory.current() is the root of the project.
+  // So check for existance of /bin/hop_runner.dart
+  final thisFile = new File('tool/hop_runner.dart');
+  assert(thisFile.existsSync());
+}
